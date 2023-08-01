866ccd9390b971f819a2d3d7facb47096e5af2d4
pixelass <greg@pixelass.com>
2023-08-01 02:18:51 +0200

    feat(groups): freeze all

--- extensions/groups.js
+++ extensions/groups.js
@@ -19,12 +19,12 @@ const groupsName = "Failfast.groups";
 
 app.registerExtension({
   name: groupsName,
-  async setup() {
-    const getGroupMenuOptions = LGraphCanvas.prototype.getGroupMenuOptions;
+  async setup(app) {
     const move = {};
     app.graph._groups.forEach((group, index) => {
       group.id = index;
     });
+    const getGroupMenuOptions = LGraphCanvas.prototype.getGroupMenuOptions;
     LGraphCanvas.prototype.getGroupMenuOptions = function (group) {
       const menuOptions = getGroupMenuOptions.apply(this, arguments);
       menuOptions.push(
@@ -53,6 +53,46 @@ app.registerExtension({
                 });
               },
             },
+      );
+
+      return menuOptions;
+    };
+    const getCanvasMenuOptions = LGraphCanvas.prototype.getCanvasMenuOptions;
+    LGraphCanvas.prototype.getCanvasMenuOptions = function () {
+      const menuOptions = getCanvasMenuOptions.apply(this, arguments);
+
+      menuOptions.push(
+        null,
+        {
+          content: "Freeze all",
+          callback: () => {
+            app.graph._groups.forEach((group) => {
+              group.recomputeInsideNodes();
+              if (!move[group.id]) {
+                move[group.id] = group.move;
+                group.move = () => {};
+              }
+              group._nodes.forEach((node) => {
+                node.flags.pinned = true;
+              });
+            });
+          },
+        },
+        {
+          content: "Unfreeze all",
+          callback: () => {
+            app.graph._groups.forEach((group) => {
+              group.recomputeInsideNodes();
+              if (move[group.id]) {
+                group.move = move[group.id];
+                move[group.id] = null;
+              }
+              group._nodes.forEach((node) => {
+                node.flags.pinned = false;
+              });
+            });
+          },
+        },
       );
 
       return menuOptions;
